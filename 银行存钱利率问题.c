f697abe070faa97050f0d269c619feafac1304af
Courage-YY <66828179+Courage-YY@users.noreply.github.com>
2020-06-18 17:36:04 +0800

    Add files via upload

---
+++ 银行存钱利率问题.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a;
+    float total,lv;
+    int year;
+    printf("请输入年限:");
+    scanf("%d",&year);
+    printf("请输入本金：");
+    scanf("%f",&a);
+    switch(year)
+    {
+    case 1:
+        lv=0.0225;
+        break;
+    case 2:
+        lv=0.0243;
+        break;
+    case 3:
+        lv=0.0268;
+        break;
+    case 5:
+        lv=0.0395;
+        break;
+    default:
+        printf("输入有误！\n");
+        break;
+    }
+    total = a*pow(1+lv,year);
+    printf("最后获取金额：%6.2f\n",total);
+    return 0;
+}
