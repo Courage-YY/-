e195fdada690d64b06a6f45c153396372c97ee4b
Courage-YY <66828179+Courage-YY@users.noreply.github.com>
2020-06-19 16:01:44 +0800

    Add files via upload

---
+++ do while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i,n,sum;
+   sum=0;
+   i=1;
+   printf("ÇëÊäÈënµÄÖµ");
+   scanf("%d",&n);
+   do
+   {
+      sum=sum+i;
+      i++;
+   }
+   while(i<=n);
+
+    printf("sum= %d\n",sum);
+    return 0;
+}
