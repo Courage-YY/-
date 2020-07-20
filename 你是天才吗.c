280366476971450767204a9935ea66d5801eb2e9
Courage-YY <66828179+Courage-YY@users.noreply.github.com>
2020-07-20 16:54:38 +0800

    Add files via upload

---
+++ 你是天才吗.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int s;
+    while(scanf("%d",&s)!=EOF)
+         if(s>=140)
+   {
+             printf("Genius");
+
+    }
+     return 0;
+}
+//据说智商140以上者称为天才，KiKi想知道他自己是不是天才，请帮他编程判断。输入一个整数表示一个人的智商，如果大于等于140，则表明他是一个天才，输出“Genius”。
